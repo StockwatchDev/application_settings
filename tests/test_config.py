2423acc0d7c1e7f985709d87c34cb6268ccc63e6
Theo de Vries <3439143+tjadevries@users.noreply.github.com>
2023-01-07 16:36:06 +0100

    add test for wrong type

--- tests/test_config.py
+++ tests/test_config.py
@@ -59,6 +59,28 @@ def test_config(monkeypatch: pytest.MonkeyPatch) -> DummyConfig:
     return DummyConfig.get()
 
 
+@pytest.fixture
+def test_config2(monkeypatch: pytest.MonkeyPatch) -> DummyConfig:
+    # here do monkeypatching of get_app_basename and _get_stored_config
+
+    def mock_get_configfile_path() -> Path:
+        return Path(__file__)
+
+    def mock_tomllib_load(
+        fptr: Any,  # pylint: disable=unused-argument
+    ) -> dict[str, dict[str, str | int]]:
+        return {
+            "section1": {
+                "field1": "f1",
+                "field2": "22",
+            }
+        }
+
+    monkeypatch.setattr(DummyConfig, "get_configfile_path", mock_get_configfile_path)
+    monkeypatch.setattr(tomllib, "load", mock_tomllib_load)
+    return DummyConfig.get(reload=True)
+
+
 def test_get_config_path() -> None:
     assert DummyConfig.get_configfile_path().parts[-1] == "config.toml"
     assert DummyConfig.get_configfile_path().parts[-2] == ".dummyconfig"
@@ -67,6 +89,10 @@ def test_get_config_path() -> None:
 def test_get(test_config: DummyConfig) -> None:
     assert test_config.section1.field1 == "f1"
     assert DummyConfig.get().section1.field2 == 22
+
+
+def test_wrong_type(test_config2: DummyConfig) -> None:
+    assert isinstance(test_config2.section1.field2, str)
 
 
 def test_get_defaults() -> None:
