4bc8b2f5aba5bd3a77b6125752eeb01b131e3cee
Theo de Vries <3439143+tjadevries@users.noreply.github.com>
2023-04-13 22:58:10 +0200

    cleanup

--- src/application_settings/container_section_base.py
+++ src/application_settings/container_section_base.py
@@ -1,6 +1,6 @@
-"""Base classes for containers and sections for configuration and settings."""
+"""Base class for sections to be added to containers and container sections for configuration and settings."""
 import sys
-from dataclasses import fields, replace
+from dataclasses import replace
 from typing import Any, TypeVar
 
 from pydantic.dataclasses import dataclass
@@ -19,63 +19,9 @@ class ContainerSectionBase:
 
         def _update(self, changes: dict[str, dict[str, Any]]) -> Self:
             "Update parameters and sections with data specified in changes; not meant for config"
-            # filter out fields that are in changes and an attribute of the Container
-            fields_to_update = {
-                fld for fld in fields(self) if fld.init and fld.name in changes.keys()
-            }
-            # # filter out fields that are a section
-            # sections_to_update = {
-            #     fld for fld in fields_to_update if isinstance(fld, ContainerSectionBase)
-            # }
-
-            # # update these sections and keep them in a dict
-            # # actually sections: dict[str, _ContainerSectionT]
-            # # but MyPy doesn't swallow that
-            # updated_sections: dict[str, Self] = {
-            #     fld.name: replace(getattr(self, fld.name), **changes[fld.name])
-            #     for fld in sections_to_update
-            # }
-
-            # # filter out fields that are not a section
-            # parameters_to_update: dict[str, Any] = {
-            #     fld.name: fld
-            #     for fld in fields_to_update
-            #     if not isinstance(fld, ContainerSectionBase)
-            # }
-            # update_values = parameters_to_update | updated_sections
-
-            # new_settings = replace(self, **update_values)
-            new_settings = replace(self, **changes)
-            return new_settings
+            return replace(self, **changes)
 
     else:
 
         def _update(self: Self, changes: dict[str, dict[str, Any]]) -> Self:
-            "Update parameters and sections with data specified in changes; not meant for config"
-            # filter out fields that are in changes and an attribute of the Container
-            fields_to_update = {
-                fld for fld in fields(self) if fld.init and fld.name in changes.keys()
-            }
-            # filter out fields that are a section
-            sections_to_update = {
-                fld for fld in fields_to_update if isinstance(fld, ContainerSectionBase)
-            }
-
-            # update these sections and keep them in a dict
-            # actually sections: dict[str, _ContainerSectionT]
-            # but MyPy doesn't swallow that
-            updated_sections: dict[str, Self] = {
-                fld.name: replace(getattr(self, fld.name), **changes[fld.name])
-                for fld in sections_to_update
-            }
-
-            # filter out fields that are not a section
-            parameters_to_update: dict[str, Any] = {
-                fld.name: fld
-                for fld in fields_to_update
-                if not isinstance(fld, ContainerSectionBase)
-            }
-            update_values = parameters_to_update | updated_sections
-
-            new_settings = replace(self, **update_values)
-            return new_settings
+            return replace(self, **changes)
