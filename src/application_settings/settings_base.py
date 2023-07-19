dc1abe0fbf2a5b847d41e36c27469723124c189e
Theo de Vries <3439143+tjadevries@users.noreply.github.com>
2023-07-19 23:34:04 +0200

    don't do update in settings section either

--- src/application_settings/settings_base.py
+++ src/application_settings/settings_base.py
@@ -1,7 +1,7 @@
 """Module for handling settings."""
 import sys
 from dataclasses import replace
-from typing import Any, TypeVar, cast
+from typing import Any, TypeVar
 
 from application_settings.container_base import ContainerBase, FileFormat
 from application_settings.container_section_base import (
@@ -18,10 +18,6 @@ else:
 SettingsT = TypeVar("SettingsT", bound="SettingsBase")
 SettingsT.__doc__ = "Represents SettingsBase and all subclasses"
 
-_SettingsOrSettingsSectionT = TypeVar(
-    "_SettingsOrSettingsSectionT", "SettingsBase", "SettingsSectionBase"
-)
-
 
 class SettingsSectionBase(ContainerSectionBase):
     """Base class for all SettingsSection classes (so that we can bound a TypeVar)"""
@@ -30,14 +26,6 @@ class SettingsSectionBase(ContainerSectionBase):
     def kind_string(cls) -> SectionTypeStr:
         """Return 'Settings'"""
         return "Settings"
-
-    @classmethod
-    def update(cls, changes: dict[str, Any]) -> Self:
-        """Update the settings with data specified in changes."""
-        updated_settings_section = cast(
-            Self, _update_settings_section(cls.get(), changes)
-        )
-        return updated_settings_section._set()  # pylint: disable=protected-access
 
 
 class SettingsBase(ContainerBase):
@@ -61,17 +49,18 @@ class SettingsBase(ContainerBase):
 
         * RuntimeError: if filepath() == None
         """
-        updated_settings_section = cast(
-            Self, _update_settings_section(cls.get(), changes)
-        )
         return (
-            updated_settings_section._set()._save()  # pylint: disable=protected-access
+            _update_settings_section(  # pylint: disable=protected-access
+                cls.get(), changes
+            )
+            ._set()
+            ._save()
         )
 
 
 def _update_settings_section(
-    the_section: _SettingsOrSettingsSectionT, changes: dict[str, Any]
-) -> _SettingsOrSettingsSectionT:
+    the_section: SettingsT, changes: dict[str, Any]
+) -> SettingsT:
     "Update parameters and sections with data specified in changes"
     # in the_section._set(), which normally is always executed, we ensured that
     # the_section is a dataclass instance
