01362eddd463741f32fede96120d4632f6d27e42
Theo de Vries <3439143+tjadevries@users.noreply.github.com>
2023-07-19 23:19:40 +0200

    do not provide update for config (Liskov)

--- src/application_settings/settings_base.py
+++ src/application_settings/settings_base.py
@@ -1,5 +1,7 @@
 """Module for handling settings."""
-from typing import TypeVar
+import sys
+from dataclasses import replace
+from typing import Any, TypeVar, cast
 
 from application_settings.container_base import ContainerBase, FileFormat
 from application_settings.container_section_base import (
@@ -7,8 +9,18 @@ from application_settings.container_section_base import (
     SectionTypeStr,
 )
 
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
+
+
 SettingsT = TypeVar("SettingsT", bound="SettingsBase")
 SettingsT.__doc__ = "Represents SettingsBase and all subclasses"
+
+_SettingsOrSettingsSectionT = TypeVar(
+    "_SettingsOrSettingsSectionT", "SettingsBase", "SettingsSectionBase"
+)
 
 
 class SettingsSectionBase(ContainerSectionBase):
@@ -18,6 +30,14 @@ class SettingsSectionBase(ContainerSectionBase):
     def kind_string(cls) -> SectionTypeStr:
         """Return 'Settings'"""
         return "Settings"
+
+    @classmethod
+    def update(cls, changes: dict[str, Any]) -> Self:
+        """Update the settings with data specified in changes."""
+        updated_settings_section = cast(
+            Self, _update_settings_section(cls.get(), changes)
+        )
+        return updated_settings_section._set()  # pylint: disable=protected-access
 
 
 class SettingsBase(ContainerBase):
@@ -32,3 +52,27 @@ class SettingsBase(ContainerBase):
     def default_file_format(cls) -> FileFormat:
         """Return the default file format"""
         return FileFormat.JSON
+
+    @classmethod
+    def update(cls, changes: dict[str, Any]) -> Self:
+        """Update the settings with data specified in changes and save.
+
+        Raises:
+
+        * RuntimeError: if filepath() == None
+        """
+        updated_settings_section = cast(
+            Self, _update_settings_section(cls.get(), changes)
+        )
+        return (
+            updated_settings_section._set()._save()  # pylint: disable=protected-access
+        )
+
+
+def _update_settings_section(
+    the_section: _SettingsOrSettingsSectionT, changes: dict[str, Any]
+) -> _SettingsOrSettingsSectionT:
+    "Update parameters and sections with data specified in changes"
+    # in the_section._set(), which normally is always executed, we ensured that
+    # the_section is a dataclass instance
+    return replace(the_section, **changes)  # type: ignore[type-var]
