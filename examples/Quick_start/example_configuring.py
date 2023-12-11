7dae425e9a420a4bbe78cb01d667fd2cdc1b9b46
Theo de Vries <3439143+tjadevries@users.noreply.github.com>
2023-12-11 22:06:42 +0100

    addition of attributes_doc

--- examples/Quick_start/example_configuring.py
+++ examples/Quick_start/example_configuring.py
@@ -9,25 +9,32 @@ from pathlib import Path
 from application_settings import (
     ConfigBase,
     ConfigSectionBase,
+    attributes_doc,
     config_filepath_from_cli,
     dataclass,
 )
 
 
+@attributes_doc
 @dataclass(frozen=True)
 class MyExampleConfigSection(ConfigSectionBase):
     """Config section for an example"""
 
     field1: float = 0.5
+    """Docstring for the first field; defaults to 0.5"""
     field2: int = 2
+    """Docstring for the second field; defaults to 2"""
 
 
+@attributes_doc
 @dataclass(frozen=True)
 class MyExampleConfig(ConfigBase):
     """Config for an example"""
 
     name: str = "nice example"
+    """With this parameter you can configure the name; defaults to 'nice example'"""
     section1: MyExampleConfigSection = MyExampleConfigSection()
+    """Holds the configuration parameters for the first section"""
 
 
 # It is good practice to set the filepath via the command line interface
