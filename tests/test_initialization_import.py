f97101d4cb12f517ccfcb221ef2bb4c503d888a8
Theo de Vries <3439143+tjadevries@users.noreply.github.com>
2024-01-01 12:21:24 +0100

    pylint fixes

--- tests/test_initialization_import.py
+++ tests/test_initialization_import.py
@@ -9,6 +9,7 @@ import tomlkit
 
 @pytest.fixture(scope="session")
 def toml_file_init(tmp_path_factory: pytest.TempPathFactory) -> Path:
+    """Create a toml file to test global initialization"""
     file_path = tmp_path_factory.mktemp(".example_init") / "config.toml"
     with file_path.open(mode="w") as fptr:
         tomlkit.dump(
@@ -21,11 +22,15 @@ def toml_file_init(tmp_path_factory: pytest.TempPathFactory) -> Path:
 
 
 def test_global_initialization(
-    monkeypatch: pytest.MonkeyPatch, toml_file_init: Path
+    monkeypatch: pytest.MonkeyPatch,
+    toml_file_init: Path,  # pylint: disable=redefined-outer-name
 ) -> None:
+    """Test global initialization"""
     monkeypatch.setattr(
         sys, "argv", ["test_global_initialization", "-a", str(toml_file_init)]
     )
-    from .config_example_module_global import test_global
+    from .config_example_module_global import (  # pylint: disable=import-outside-toplevel
+        test_global,
+    )
 
     assert test_global == 33.33
